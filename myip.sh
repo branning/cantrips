8fa31a815ab17e42c9e7b38174bd51463731dd04
Philip Branning <branning@gmail.com>
2016-09-04 11:22:44 -0700

    add support for Windows with ipconfig

--- myip.sh
+++ myip.sh
@@ -2,7 +2,21 @@
 #
 # Find the local IP by munging ipconfig output
 
-ifconfig \
-    | grep '^\s*inet[^6]' \
-    | grep -v '127.0.0.1' \
-    | awk '{ print $2 }'
+# windows has ipconfig
+case `basename $(type -p ipconfig ifconfig)` in
+ipconfig)
+    ipconfig \
+        | grep 'IPv4 Address' \
+        | grep -v '127.0.0.1' \
+        | egrep -o '(([0-9]{1,3}\.){3}[0-9]{1,3})'
+    ;;
+ifconfig)
+    ifconfig \
+        | grep '^\s*inet[^6]' \
+        | grep -v '127.0.0.1' \
+        | awk '{ print $2 }'
+    ;;
+*)  echo 2>&1 "Must have ipconfig or ifconfig available to get IP address"
+    exit 1
+    ;;
+esac
