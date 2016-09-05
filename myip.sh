c9392eec6f5722da5b0fd77303ebff7b2e4a05fe
Philip Branning <pbranning@oblong.com>
2016-09-05 16:34:19 -0700

    okay fix osx support

--- myip.sh
+++ myip.sh
@@ -3,20 +3,23 @@
 # Find the local IP by munging ipconfig output
 
 # windows has ipconfig
-case `basename $(type -p ipconfig ifconfig)` in
-ipconfig)
+ipconfig=`command -v ipconfig`
+ifconfig=`command -v ifconfig`
+# windows has ipconfig but doesn't have ifconfig
+if [ -n $ipconfig ] && ! [ -n $ifconfig ]
+then
     ipconfig \
         | grep 'IPv4 Address' \
         | grep -v '127.0.0.1' \
         | egrep -o '(([0-9]{1,3}\.){3}[0-9]{1,3})'
-    ;;
-ifconfig)
+# osx and linux have ifconfig
+elif [ -n $ifconfig ]
+then
     ifconfig \
         | grep '^\s*inet[^6]' \
         | grep -v '127.0.0.1' \
         | awk '{ print $2 }'
-    ;;
-*)  echo 2>&1 "Must have ipconfig or ifconfig available to get IP address"
+else
+    echo 2>&1 "Must have ipconfig or ifconfig available to get IP address"
     exit 1
-    ;;
-esac
+fi
