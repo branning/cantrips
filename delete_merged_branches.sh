36c4f9b4be93c6cd8154396dc93e489834d31bb3
Philip Branning <branning@gmail.com>
2020-09-21 10:01:46 -0700

    git delete merged branches

---
+++ delete_merged_branches.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# Delete merged branches, but only if they are not special branches
+
+TEST=${TEST-'echo'}
+
+SPECIAL='
+  master
+  develop
+  \*
+  '
+
+join_by() { local IFS="$1"; shift; echo "$*"; }
+
+git branch --merged |
+  grep -vE "(`join_by '|' $SPECIAL`)" |
+  xargs $TEST git branch -d
+
+if ! [ -z ${TEST-x} ]; then
+  echo "* just a test, to really delete run TEST= ${BASH_SOURCE}"
+fi
+
